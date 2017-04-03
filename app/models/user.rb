9e95f4512c1828b2c78d4576d850a8c67fb0cf35
Deepak <deepak.jpjl@gmail.com>
2017-04-03 19:03:34 +0000

    Added users model + Authentication

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    before_save {self.email = self.email.downcase}
+    validates :name, presence: true, length: {maximum: 30}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 30}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password, presence: true, length: {minimum: 6, maximum: 12}
+end
